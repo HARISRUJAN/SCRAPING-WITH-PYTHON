93e1b87b34aa1f869c3ed239a58b8657e2e8eda8
HARISRUJAN <65223347+HARISRUJAN@users.noreply.github.com>
2020-06-22 22:11:01 +0530

    SPIDER THAT SCRAPS WIKIPEDIA
    This spider scraps data from wiki page

---
+++ wiki/wiki/pipelines.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+import pymongo
+
+class WikiPipeline:
+
+    def __init__(self):
+        self.conn=pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+
+        db=self.conn['wiki']
+        self.collection=db['wiki_tb']
+    def process_item(self, item, spider):
+        self.collection.insert(dict(item))
+        return item
